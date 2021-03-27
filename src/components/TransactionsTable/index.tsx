42494945f9413b0c46a33dffd633376709cf1deb
jaf88 <jaf.alves8@gmail.com>
2021-03-27 17:58:25 -0300

    feat: Context and hook

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,10 @@
+import { useTransactions } from "../../hooks/useTransactions";
+
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  const { transactions } = useTransactions();
+
   return (
     <Container>
       <table>
@@ -14,18 +18,23 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de WebSite</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>25/03/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw"> - R$1.000</td>
-            <td>Casa</td>
-            <td>20/03/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date (transaction.createdAt)
+                )}
+              </td>
+            </tr>  
+          ))}
         </tbody>
       </table>      
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createServer, Model } from 'miragejs';
+
 import App from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer Mobile App',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-06-01 08:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 200,
+          createdAt: new Date('2021-06-01 08:00:00'),
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+    
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
+    });
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
